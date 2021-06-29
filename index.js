52f4781fbe20a418b524b5a2ec104003bc45a3d5
Philipp Brendel <waldrumpus@gmail.com>
2021-06-29 13:25:05 +0200

    add api route

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const HTTP_PUBLIC = '../MdBlog/dist/MdBlog/';
+const API = '/api/v1'
+const POSTS = '../posts/';
 const port = 8080;
 
 const bodyParser = require('body-parser');
@@ -7,13 +9,15 @@ const express = require('express');
 
 var app = express();
 
+/////////// blog frontend ///////////
+
 app
     .use(compression())
     .use(bodyParser.json())
     // Static content
     .use(express.static(HTTP_PUBLIC))
     // Default route
-    .use(function(req, res) {
+    .get('/', function(req, res) {
       res.sendFile(HTTP_PUBLIC + 'index.html');
     })
     // Start server
@@ -21,3 +25,9 @@ app
         console.log('Port: ' + port);
         console.log('Html: ' + HTTP_PUBLIC);
     });
+
+/////////// API ///////////
+
+app.get(`${API}/:id`, (req, res) => {
+    res.send(`Fetching blog post ${req.params.id}`);
+});
